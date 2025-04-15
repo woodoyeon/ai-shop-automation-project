0eb9f5f92ce75a5f73e2a9881268940eb111704f
woodoyeon <dydy1212qwqw@naver.com>
2025-04-16 01:55:26 +0900

    💻 학교에서 이미지 분석 추가

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+// client/src/App.jsx
+import React from "react";
+import Home from "./pages/Home"; // 또는 원하는 컴포넌트
+
+function App() {
+  return (
+    <div className="App">
+      <Home /> {/* 여기에 기존 코드 컴포넌트 삽입 */}
+    </div>
+  );
+}
+
+export default App;
--- client/vite.config.js
+++ client/vite.config.js
@@ -1,4 +1,6 @@
-import react from '@vitejs/plugin-react';
-export default {
-  plugins: [react()]
-};
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+})
